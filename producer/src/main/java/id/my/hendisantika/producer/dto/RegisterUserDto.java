b38fa56bc81c1fa571232d6bd0f69f2e761d04ef
hendisantika <hendisantika@yahoo.co.id>
2024-06-10 06:53:22 +0700

    Add RegisterUserDto 🫸🌀✏️📗🐧🐳

---
+++ producer/src/main/java/id/my/hendisantika/producer/dto/RegisterUserDto.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.producer.dto;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rabbitmq-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/10/24
+ * Time: 06:53
+ * To change this template use File | Settings | File Templates.
+ */
+public record RegisterUserDto(String name, String email) {
+}
